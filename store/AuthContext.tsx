1a653538b5c021a7d2fcd8f6382d80e32335a6ab
Nicky Hendrik Sen <nicky.hendriksen@yahoo.com>
2023-04-06 23:41:09 +0700

    Removed comments

--- store/AuthContext.tsx
+++ store/AuthContext.tsx
@@ -1,15 +1,10 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react"
 import { getCookie, removeCookies, setCookies } from 'cookies-next';
 import { APISettings } from "../api/base";
-import { useAsync } from "../utils/useAsync";
-// import { CompanyAPI } from "../api/apis/companyApi";
 
 const TOKEN_NAME = "token"
 
 export const AuthContext = createContext<null | {
-    // userToken: null | string,
-    // company: Boolean,
-    // companyInitialized: Boolean,
     login: (token: string | null) => unknown,
     logout: () => unknown,
     isAuthenticated: () => unknown
@@ -18,25 +13,16 @@ export const AuthContext = createContext<null | {
 export const AuthContextProvider = (props: { children: ReactNode }) => {
     
     const [ token, setToken ] = useState<string | null>(null)
-    // const [ company, setCompany ] = useState<Boolean>(true)
     const [ loaded, setLoaded ] = useState(false);
-    // const { execute, value } = useAsync(CompanyAPI.company)
     
     const handleLogout = () => {
         setToken(null)
         removeCookies(TOKEN_NAME)
     }
 
-    // useEffect(() => {
-    //     if(value){
-    //         setCompany(!!value?.data?.companyDetails);
-    //     }
-    // }, [value])
-
     useEffect(() => {
         if (token) {
             APISettings.setAPIToken(token, handleLogout)
-            // execute({});
             setLoaded(true)
         }
     }, [token])
