a4faafde27ef2591f317944f791ad0cf0fa0218e
unknown <nicky.hendriksen@yahoo.com>
2023-05-01 11:34:41 +0700

    Re commit

--- components/form/FormErrorTextStyles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const ErrorText = styled.div<{fontSize: string}>`
-  color: white;
-  margin-top: 5px;
-  font-size: ${props => props.fontSize ?? "15px"};
-`
