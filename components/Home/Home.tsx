c324f5436ad0fd8099a53fcee08d18dc7fa6993d
unknown <nicky.hendriksen@yahoo.com>
2023-05-05 16:02:30 +0700

    Added home page content

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,29 @@
+import { TitleText } from "@/styles/Typography";
 import Container from "../Template/Container";
-// import {
-// } from "./Styles"
+import {
+  DescriptionText,
+  LinkText
+} from "./Styles"
+import Link from "next/link";
 
 const Home: React.FC = () => {
+  const githubLink = "https://github.com/NickyHendrikSen"
+  const linkedinLink = "https://www.linkedin.com/in/nicky-hendrik-sen-4883b2174/"
   return (
-    <Container>
-      
+    <Container paddingTop="50px">
+      <TitleText>This is a home page</TitleText>
+      <DescriptionText>This is a portfolio website made by Nicky Hendrik Sen</DescriptionText>
+      <LinkText>Visit my LinkedIn <Link href={linkedinLink}>here</Link></LinkText>
+      <LinkText>Visit my GitHub <Link href={githubLink}>here</Link></LinkText>
+      <br />
+      <DescriptionText>Techs : Next.js, Node.js, MongoDB, and Mongoose.</DescriptionText>
+      <br />
+      <DescriptionText>Stripe checkout that is used in this website is a test environment meaning that you won't get charged, 
+        However you can checkout using this test cards provided by Stripe : 
+        <br />Number : 4242 4242 4242 4242
+        <br />CVC : Any 3 digits
+        <br />Date : Any future date
+        </DescriptionText>
     </Container>
   )
 }
--- components/Home/Styles.ts
+++ components/Home/Styles.ts
@@ -1,1 +1,16 @@
+import styled from "styled-components";
 
+export const DescriptionText = styled.div`
+
+`
+
+export const LinkText = styled.div`
+  a {
+    color: var(--color-blue);
+    font-weight: bold;
+    text-decoration: none;
+    &: hover {
+      color: var(--color-light-blue);
+    }
+  }
+`
